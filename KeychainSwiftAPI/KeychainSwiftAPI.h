ab77309847d036789affa198c3c61674a5f0b6ae
Denis Krivitski <denis.krivitski@gmail.com>
2014-07-22 16:45:30 +0300

    Initial Commit

---
+++ KeychainSwiftAPI/KeychainSwiftAPI.h
@@ -0,0 +1,19 @@
+//
+//  KeychainSwiftAPI.h
+//  KeychainSwiftAPI
+//
+//  Created by Denis Krivitski on 22/7/14.
+//  Copyright (c) 2014 Checkmarx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeychainSwiftAPI.
+FOUNDATION_EXPORT double KeychainSwiftAPIVersionNumber;
+
+//! Project version string for KeychainSwiftAPI.
+FOUNDATION_EXPORT const unsigned char KeychainSwiftAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeychainSwiftAPI/PublicHeader.h>
+
+
